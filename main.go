103adc68acc2bf59624e6c7bff9d22c009c13662
pitasz <pita@local>
2023-01-10 14:29:38 +0800

    Initial commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"golang.org/x/net/html/charset"
+	"golang.org/x/text/encoding"
+	"golang.org/x/text/encoding/unicode"
+	"golang.org/x/text/transform"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+)
+
+func mainDpre() {
+	url := "https://www.thepaper.cn/"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("fether url error:%v", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		fmt.Println("Error status code: %v", resp.StatusCode)
+		return
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("ReadAll error:", err)
+		return
+	}
+
+	//fmt.Println("body:", string(body))
+	n := strings.Count(string(body), "<a")
+	bn := bytes.Count(body, []byte("<a"))
+	if n != bn {
+		fmt.Printf("n: %d, bn: %d\n", n, bn)
+	}
+	fmt.Println("contains url: ", n)
+
+}
+
+func init() {
+	log.SetPrefix("HTTP DEV: ")
+	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Lshortfile)
+}
+
+func main() {
+	url := "https://www.thepaper.cn/"
+
+	fetch, err := Fetch(url)
+	if err != nil {
+		log.Println("Failed to fetch url:", err)
+		return
+	}
+
+	//var headerRe = regexp.MustCompile(`<div class="news_cli"[\s\S]*?<h2>[\s\S]*?<a.*?target="_blank">([\s\S]*?)</a>`)
+	var headerRe = regexp.MustCompile(`<div class="small_toplink__GmZhY"[\s\S]*?<h2>([\s\S]*?)</h2>`)
+	matches := headerRe.FindAllSubmatch(fetch, -1)
+	for _, m := range matches {
+		log.Println("fetch card news:", string(m[1]))
+	}
+}
+
+func Fetch(url string) ([]byte, error) {
+
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		log.Println("not the expected status:", resp.StatusCode)
+	}
+
+	reader := bufio.NewReader(resp.Body)
+	e := DeterminEncoding(reader)
+	utf8Reader := transform.NewReader(reader, e.NewDecoder())
+	return ioutil.ReadAll(utf8Reader)
+}
+
+func DeterminEncoding(r *bufio.Reader) encoding.Encoding {
+	bytes, err := r.Peek(1024)
+
+	if err != nil {
+		log.Println("fetch error:", err)
+		return unicode.UTF8
+	}
+
+	e, _, _ := charset.DetermineEncoding(bytes, "")
+	return e
+}
